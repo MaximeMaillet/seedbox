90e7854f2d1566a31a6958e9224a529ecadc3fb2
Maxime Maillet <maxime.maillet93@gmail.com>
2017-12-16 01:55:40 +0100

    fix docker + upgrade dtorrent

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,22 @@
 FROM debian:jessie
 
-ARG UUID=1000
-ARG GGID=1000
-ARG USER=torrent
+ENV USER=torrent
 
 RUN apt-get update && \
     apt-get install -y rtorrent \
     supervisor \
     nginx
 
-RUN if [ ! -z $(getent group $GGID) ] ; then groupmod -o -g 2019292 $(getent group $GGID | cut -d: -f1) ; fi && \
-    addgroup --system --gid $GGID $USER && \
-    if [ ! -z $(getent passwd $UUID) ] ; then usermod -o -u 2019292 $(getent passwd $UUID | cut -d: -f1) ; fi && \
-    useradd -l --system --home-dir /home/$USER --uid $UUID --gid $GGID $USER
-
-RUN mkdir -p /var/rtorrent/session /var/rtorrent/torrents /var/rtorrent/downloaded /var/rtorrent/logs && \
-    chown -R root. /var/rtorrent
+RUN mkdir -p /home/$USER && \
+    addgroup --system --gid 1000 $USER && \
+    useradd -l --system --home-dir /home/$USER --uid 1000 --gid 1000 $USER && \
+    mkdir -p /var/rtorrent/session /var/rtorrent/torrents /var/rtorrent/downloaded /var/rtorrent/logs && \
+    mkdir -p /var/log/supervisor && \
+    chown -R $USER. /var/rtorrent && \
+    chown -R $USER. /home/$USER
 
 ADD Docker/supervisor /etc/supervisor
-ADD Docker/nginx/conf.d /etc/nginx/conf.d
-ADD Docker/rtorrent /root
-
-RUN mkdir -p /var/log/supervisor
+ADD Docker/nginx/sites-enabled/default.conf /etc/nginx/sites-enabled/default
+ADD Docker/rtorrent /home/$USER
 
 CMD ["/usr/bin/supervisord"]
--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 const dtorrent = require('dtorrent');
 
 const dConfig = {
-	torrent: {
-		client: 'rTorrent', // Client for download torrent
-		host: '127.0.0.1', // IP of client torrent
-		port: 8092, // Port of client torrent
-		path: '/RPC2' // Path to join client torrent via XML RPC
-	}
+	rtorrent_host: '127.0.0.1', // IP of client torrent
+	rtorrent_port: 8092, // Port of client torrent
+	rtorrent_path: '/RPC2' // Path to join client torrent via XML RPC
 };
 
-var dListener = {
+const dListener = {
 	/**
 	 * Function called when torrent is inserted
 	 * @param torrent
 	 */
 	onInsert: function(torrent) {
-		console.log("Event insert %s", torrent.hash);
+		console.log('client Event insert %s', torrent.hash);
 	},
 	/**
 	 * Function called when torrent is uploading
 	 * @param torrent
 	 */
-	onUploaded: function(torrent) {
-		console.log("event uploaded %s", torrent.hash);
+	onUpload: function(torrent) {
+		console.log('client event uploaded %s', torrent.hash);
 	},
 	/**
 	 * Function called when torrent is downloading
 	 * @param torrent
 	 */
-	onDownloaded: function(torrent) {
-		console.log("event download %s", torrent.hash);
+	onDownload: function(torrent) {
+		console.log('client event download %s', torrent.hash);
 	},
 	/**
 	 * Function called when torrent is finished
 	 * @param torrent
 	 */
 	onFinished: function(torrent) {
-		console.log("event on finished %s", torrent.hash)
+		console.log('client event on finished %s', torrent.hash);
 	}
 };
 
-dtorrent.start(dConfig, dListener);
+dtorrent.addConfig(dConfig);
+
+dtorrent.start(dListener);
