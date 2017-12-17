4d466fa0f917ca23cae975564f7e6a8c36ee77b6
Maxime Maillet <maxime.maillet93@gmail.com>
2017-12-18 00:55:05 +0100

    fix

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ module.exports = (_express) => {
 			'host': process.env.MYSQL_HOST,
 			'user': process.env.MYSQL_USER,
 			'password': process.env.MYSQL_PASSWORD,
-			'database': process.env.DATABASE,
+			'database': process.env.MYSQL_DATABASE,
 			'dialect': 'mysql'
 		}
 	});
