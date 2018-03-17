e0b74652ca276766ab7c95fd6ba52415f1a6f8ec
Maxime Maillet <maxime.maillet93@gmail.com>
2018-03-17 21:11:58 +0100

    clean legacy

--- src/front/twig/is-granted.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const userService = require('../../services/user');
-
-module.exports.name = 'is_granted';
-
-module.exports.main = (user, roles) => {
-	return userService.isGranted(user, roles);
-};
